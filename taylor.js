3ec88a9b47c0cb474e63dc4ebf782159cd0dcd69
Raj (Springboard) <raj@springboard.com>
2016-03-20 09:39:22 -0700

    Added http call every 5 mins

--- taylor.js
+++ taylor.js
@@ -27,6 +27,9 @@ http.createServer(function (req, res) {
   res.send('it is running\n'); }
 ).listen(process.env.PORT || 5000);
 
+setInterval(function() {
+    http.get("http://sleepy-dusk-64526.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
 
 /**
  * Configure the persistence options
